a0c940fab1090574010b8b435df0204f7271bca9
Jose Miranda <josevitormiranda96@gmail.com>
2026-06-27 19:15:38 -0300

    updating the repository with date formatation, to better views on the system

--- dist/js/saldo-component.js
+++ dist/js/saldo-component.js
@@ -1,5 +1,15 @@
 let saldo = 3000;
 const elementSaldo = document.querySelector(".saldo-value .value");
+const elementAccessDate = document.querySelector(".block-saldo time");
 if (elementSaldo != null) {
     elementSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency" });
+}
+if (elementAccessDate != null) {
+    const accessDate = new Date();
+    elementAccessDate.textContent = accessDate.toLocaleDateString("pt-br", {
+        weekday: "long",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
 }
